c6dcf5eceafa167863e7c08ea178d2fc539e950e
aristotleboi <gage24paul@gmail.com>
2022-09-13 01:20:58 -0700

    succefully stored and logged data from the api

--- dist/main.js
+++ dist/main.js
@@ -3,17 +3,24 @@ var __webpack_exports__ = {};
 /*!**********************!*\
   !*** ./src/index.js ***!
   \**********************/
+let weatherObject = getWeather()
+
 async function getWeather() {
-    fetch('https://api.openweathermap.org/data/2.5/weather?q=Vancouver&APPID=65d57bbf714db2778c05eb37107c6320&units=metric')
-    .then(function(response) {
-        return response.json();
-    })
-    .then(function(response) {
-        console.log(response.main)
-    })
+    try {
+        const response = await fetch('https://api.openweathermap.org/data/2.5/weather?q=Vancouver&APPID=65d57bbf714db2778c05eb37107c6320&units=metric')
+        let weatherData = await response.json()
+        weatherObject = weatherData
+    } catch(err) {
+        console.log(err)
+    }
 }
 
-getWeather()
+
+weatherObject.then(function() {
+    console.log(weatherObject)
+    console.log(weatherObject.weather[0].description)
+})
+
 /******/ })()
 ;
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi5qcyIsIm1hcHBpbmdzIjoiOzs7OztBQUFBO0FBQ0E7QUFDQTtBQUNBO0FBQ0EsS0FBSztBQUNMO0FBQ0E7QUFDQSxLQUFLO0FBQ0w7O0FBRUEsWSIsInNvdXJjZXMiOlsid2VicGFjazovL3dlYXRoZXItYXBwLy4vc3JjL2luZGV4LmpzIl0sInNvdXJjZXNDb250ZW50IjpbImFzeW5jIGZ1bmN0aW9uIGdldFdlYXRoZXIoKSB7XG4gICAgZmV0Y2goJ2h0dHBzOi8vYXBpLm9wZW53ZWF0aGVybWFwLm9yZy9kYXRhLzIuNS93ZWF0aGVyP3E9VmFuY291dmVyJkFQUElEPTY1ZDU3YmJmNzE0ZGIyNzc4YzA1ZWIzNzEwN2M2MzIwJnVuaXRzPW1ldHJpYycpXG4gICAgLnRoZW4oZnVuY3Rpb24ocmVzcG9uc2UpIHtcbiAgICAgICAgcmV0dXJuIHJlc3BvbnNlLmpzb24oKTtcbiAgICB9KVxuICAgIC50aGVuKGZ1bmN0aW9uKHJlc3BvbnNlKSB7XG4gICAgICAgIGNvbnNvbGUubG9nKHJlc3BvbnNlLm1haW4pXG4gICAgfSlcbn1cblxuZ2V0V2VhdGhlcigpIl0sIm5hbWVzIjpbXSwic291cmNlUm9vdCI6IiJ9
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi5qcyIsIm1hcHBpbmdzIjoiOzs7OztBQUFBOztBQUVBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUFDQSxNQUFNO0FBQ047QUFDQTtBQUNBOzs7QUFHQTtBQUNBO0FBQ0E7QUFDQSxDQUFDIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vd2VhdGhlci1hcHAvLi9zcmMvaW5kZXguanMiXSwic291cmNlc0NvbnRlbnQiOlsibGV0IHdlYXRoZXJPYmplY3QgPSBnZXRXZWF0aGVyKClcblxuYXN5bmMgZnVuY3Rpb24gZ2V0V2VhdGhlcigpIHtcbiAgICB0cnkge1xuICAgICAgICBjb25zdCByZXNwb25zZSA9IGF3YWl0IGZldGNoKCdodHRwczovL2FwaS5vcGVud2VhdGhlcm1hcC5vcmcvZGF0YS8yLjUvd2VhdGhlcj9xPVZhbmNvdXZlciZBUFBJRD02NWQ1N2JiZjcxNGRiMjc3OGMwNWViMzcxMDdjNjMyMCZ1bml0cz1tZXRyaWMnKVxuICAgICAgICBsZXQgd2VhdGhlckRhdGEgPSBhd2FpdCByZXNwb25zZS5qc29uKClcbiAgICAgICAgd2VhdGhlck9iamVjdCA9IHdlYXRoZXJEYXRhXG4gICAgfSBjYXRjaChlcnIpIHtcbiAgICAgICAgY29uc29sZS5sb2coZXJyKVxuICAgIH1cbn1cblxuXG53ZWF0aGVyT2JqZWN0LnRoZW4oZnVuY3Rpb24oKSB7XG4gICAgY29uc29sZS5sb2cod2VhdGhlck9iamVjdClcbiAgICBjb25zb2xlLmxvZyh3ZWF0aGVyT2JqZWN0LndlYXRoZXJbMF0uZGVzY3JpcHRpb24pXG59KVxuIl0sIm5hbWVzIjpbXSwic291cmNlUm9vdCI6IiJ9
--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
+let weatherObject = getWeather()
+
 async function getWeather() {
-    fetch('https://api.openweathermap.org/data/2.5/weather?q=Vancouver&APPID=65d57bbf714db2778c05eb37107c6320&units=metric')
-    .then(function(response) {
-        return response.json();
-    })
-    .then(function(response) {
-        console.log(response.main)
-    })
+    try {
+        const response = await fetch('https://api.openweathermap.org/data/2.5/weather?q=Vancouver&APPID=65d57bbf714db2778c05eb37107c6320&units=metric')
+        let weatherData = await response.json()
+        weatherObject = weatherData
+    } catch(err) {
+        console.log(err)
+    }
 }
 
-getWeather()
+
+weatherObject.then(function() {
+    console.log(weatherObject)
+    console.log(weatherObject.weather[0].description)
+})
