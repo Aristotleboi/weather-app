d18af68ac8e480437c0854a02dfde8892af072ab
Aristotleboi <gage24paul@gmail.com>
2022-09-15 01:10:11 -0700

    successfully displayed date that was fetched from the weather api on the website

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
+const submitButton = document.getElementById('submit')
+let citySearch = 'Vancouver';
 let weatherObject = getWeather()
 
 async function getWeather() {
     try {
-        const response = await fetch('https://api.openweathermap.org/data/2.5/weather?q=Vancouver&APPID=65d57bbf714db2778c05eb37107c6320&units=metric')
+        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${citySearch}&APPID=65d57bbf714db2778c05eb37107c6320&units=metric`)
         let weatherData = await response.json()
         weatherObject = weatherData
-    } catch(err) {
+    } catch (err) {
+        console.log('error')
         console.log(err)
     }
 }
@@ -15,3 +18,22 @@ weatherObject.then(function() {
     console.log(weatherObject)
     console.log(weatherObject.weather[0].description)
 })
+
+submitButton.addEventListener('click', () => {
+    getUserSearch()
+    getWeather()
+    .then(function(){
+        displayWeather()
+    })
+})
+
+function displayWeather() {
+    let weatherDescription = document.getElementById('weather-description');
+    weatherDescription.innerText = weatherObject.weather[0].description
+}
+
+function getUserSearch() {
+    citySearch = document.getElementById('city-search').value;
+    document.getElementById('city-search').value = '';
+}
+
